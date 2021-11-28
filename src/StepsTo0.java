aa458131fd731f2fbcd7b5cde16cc2c74aa9ef51
Hassan Chowdhry <mohammedchowdhry11@gmail.com>
2021-11-28 14:59:27 +0500

    Update StepsTo0.java

--- src/StepsTo0.java
+++ src/StepsTo0.java
@@ -1,8 +1,23 @@
 public class StepsTo0 {
 
     // TODO - needs to be impl.
-    public int numberOfSteps(int num) {
-        return 0;
+    public int numberOfSteps(int num)
+    {
+        int steps = 0;
+        if (num == 0)
+        {
+            return 0;
+        }
+        while (num > 0 )
+        {
+            if (num % 2 == 0)
+            {
+                num /= 2;
+            }
+            else num -= 1;
+            steps++;
+        }
+        return steps;
     }
 
 }
